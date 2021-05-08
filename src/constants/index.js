f9e454fcf7d74b4bfe379f67d4856c518e089917
Govind S <govindsd@peopleintouch.com>
2021-05-08 15:42:52 +0200

    cache-util: added constants

---
+++ src/constants/index.js
@@ -0,0 +1,22 @@
+/**
+ * This file contains all the constants that are used in the project.
+ */
+
+const STATUS_CODES = {
+  OK: 200,
+  CREATED: 201,
+  NO_CONTENT: 204,
+  CLIENT_ERROR: 400,
+  SERVER_ERROR: 500
+}
+
+const COLLECTIONS = {
+  CACHE_ITEM: "cache"
+}
+
+export default {
+  CACHE_TTL_IN_MILLIS: 60*1000,
+  MAX_CACHE_ITEMS: 5,
+  COLLECTIONS,
+  STATUS_CODES
+}
