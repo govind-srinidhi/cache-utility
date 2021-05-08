2e49a4d4bba96a38109318118ebb9405136e1a1a
Govind S <govindsd@peopleintouch.com>
2021-05-08 15:42:35 +0200

    cache-util: included entry point for the app

---
+++ app.js
@@ -0,0 +1,17 @@
+const express    = require("express")
+const bodyParser = require("body-parser")
+const app        = express()
+
+const routes             = require("./routes")
+const initializeDatabase = require("./src/database")
+
+initializeDatabase().then(db => {
+  routes(app, db).listen(process.env.SERVER_PORT, () => console.log(`Listening on port ${process.env.SERVER_PORT}`))
+}).catch(err => {
+  console.error("Failed to make database connection!")
+  console.error(err)
+  process.exit(1)
+})
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
