de7f1c5499cd1b4cf1bf4978f72642498a41baf9
Govind S <govindsd@peopleintouch.com>
2021-05-08 15:41:04 +0200

    cache-util: added routes for the application

---
+++ routes/index.js
@@ -0,0 +1,45 @@
+const CacheItemHandler = require("../src/handlers/CacheItemHandler")
+
+module.exports = function(app, db) {
+  app.get("/cache-items", (request, response) => {
+    CacheItemHandler.getAll(request, response, db)
+      .then(results => response.status(Constants.STATUS_CODES.OK).json(results))
+      .catch(next)
+  })
+  
+  app.get("/cache-items/:key", (request, response, next) => {
+    CacheItemHandler.findByKey(request, response, db)
+        .then(results => response.status(Constants.STATUS_CODES.OK).json(results))
+        .catch(next)
+  })
+  
+  app.post("/cache-items", (request, response, next) => {
+    CacheItemHandler.create(request, response, db)
+        .then(results => response.status(Constants.STATUS_CODES.CREATED).json(results))
+        .catch(next)
+  })
+  
+  app.put("/cache-items/:key", (request, response, next) => {
+    CacheItemHandler.update(request, response, db)
+        .then(results => response.status(Constants.STATUS_CODES.NO_CONTENT).json())
+        .catch(next)
+  })
+  
+  app.delete("/cache-items/:key", (request, response, next) => {
+    CacheItemHandler.deleteCacheItem(request, response, db)
+        .then(results => response.status(Constants.STATUS_CODES.NO_CONTENT).json())
+        .catch(next)
+  })
+  
+  app.delete("/cache-items", (request, response, next) => {
+    CacheItemHandler.deleteAllCacheItems(request, response, db)
+        .then(results => response.status(Constants.STATUS_CODES.NO_CONTENT).json())
+        .catch(next)
+  })
+  
+  app.use(function (err, req, res, next) {
+    console.log(err.stack)
+    console.log(err)
+    res.status(500).send({ErrorDetails: "" + err})
+  })
+}
